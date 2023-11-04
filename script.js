fa5c804c7a70c71be23566817dab363b8fd9f0f0
Justin <jlke45@gmail.com>
2023-11-04 16:44:56 +0900

    Add basic calculation functions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Variables for display
+let m;  // First number
+let n;  // Second numbe
+let operator;
+
+// Basic operations
 function add(a,b) {
     return a + b;
 }
@@ -12,4 +18,20 @@ function multiply(a,b) {
 
 function divide(a,b) {
     return a / b;
+}
+
+// Main operation function
+function operate(a, b, op) {
+    switch(op) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+        default:
+            console.log("Unrecognized operation");
+    }
 }
