1675ad1014ef00adc0f1c3caf8eaaf97fa42030c
Justin <jlke45@gmail.com>
2023-11-05 13:04:26 +0900

    Add functionality to display key contet to display

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Variables for display
+let displayNumber = document.querySelector(".calc-display");
 let m;  // First number
 let n;  // Second numbe
 let operator;
+const digits = document.querySelectorAll(".digit");
+const operationKeys = document.querySelector(".operation-key");
+const functionKeys = document.querySelector(".function-key");
+const allKeys = document.querySelectorAll("button");
 
 // Basic operations
 function add(a,b) {
@@ -35,3 +40,19 @@ function operate(a, b, op) {
             console.log("Unrecognized operation");
     }
 }
+
+// Display key presses
+function displayKeyPress() {
+    allKeys.forEach(key => {
+        key.addEventListener("click", () => {
+            if (key.textContent == "AC") {
+                displayNumber.textContent = "0";
+            }
+            else {
+                displayNumber.textContent = key.textContent;
+            }   
+        })
+    })
+}
+
+displayKeyPress();
